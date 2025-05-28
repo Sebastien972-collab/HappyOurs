93e1354ba04d6c4a8a1b8110767cce27cfb5159d
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-05-28 16:19:05 +0200

    First commit

---
+++ HappyOurs/Supporting Files/HappyOursApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyOursApp.swift
+//  HappyOurs
+//
+//  Created by Sébastien DAGUIN  on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HappyOursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HappyOurs/View/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  HappyOurs
+//
+//  Created by Sébastien DAGUIN  on 28/05/2025.
+//
+
+import SwiftUI
+import FestivityUIKit
+struct ContentView: View {
+    enum Selection {
+    case event, message, profile
+    }
+    @State private var selection: Selection = .event
+    
+    var body: some View {
+        ZStack {
+            TabView {
+                Tab("Évènements", systemImage: "list.bullet") {
+                    Text("Évènements")
+                }
+                Tab("Messages", systemImage: "message.fill") {
+                    Text("Messages")
+                }
+                
+                
+                Tab("Profile", systemImage: "person.fill") {
+                    Text("Profiles")
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
