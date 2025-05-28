c833a960cf603f2e96b8a5919f8d4978cb0dadb8
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2025-05-28 16:55:02 +0200

    Ajout de user et de User lanager

---
+++ HappyOurs/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  HappyOurs
+//
+//  Created by Sébastien DAGUIN  on 28/05/2025.
+//
+
+import Foundation
+
+class User: Identifiable {
+    var id = UUID()
+}
---
+++ HappyOurs/ViewModel/UserManager.swift
@@ -0,0 +1,12 @@
+//
+//  UserManager.swift
+//  HappyOurs
+//
+//  Created by Sébastien DAGUIN  on 28/05/2025.
+//
+
+import Foundation
+
+class UserManager: ObservableObject {
+     
+}
