64253141632d77f7dc4874f8b9c71dbaab4d8dd4
Dembo <diaouned@yahoo.fr>
2025-05-30 15:57:47 +0200

    Ajout de EventList

---
+++ HappyOurs/View/Event/ListEvent.swift
@@ -0,0 +1,18 @@
+//
+//  ListEvent.swift
+//  HappyOurs
+//
+//  Created by Dembo on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ListEvent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListEvent()
+}
