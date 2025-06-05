6de0d2810f85c4fc3ae1511daf94b3f732819a9a
Dembo <diaouned@yahoo.fr>
2025-06-05 15:16:34 +0200

    update evenlistview

---
+++ HappyOurs/View/Event/blockListEvent/MignatureParticipant.swift
@@ -0,0 +1,21 @@
+//
+//  MignatureParticipant.swift
+//  HappyOurs
+//
+//  Created by Dembo on 05/06/2025.
+//
+
+import SwiftUI
+
+struct MignatureParticipant: View {
+    var body: some View {
+        Image (systemName: "person.circle.fill")
+            .resizable()
+            .frame(width: 12, height: 12)
+            
+    }
+}
+
+#Preview {
+    MignatureParticipant()
+}
